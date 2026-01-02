fc9412e3feb6b26c7ee772a278585d83063ad9b9
BtwToxic <btwalexxd@gmail.com>
2026-01-02 21:41:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     API_ID = "28461830"
     API_HASH = "58935dd717d0205b8051e77205f407fb"
-    TOKEN = "8589792528:AAGk0_qxWta0-r7jq9JOgmb9hklr0AtdghU"
+    TOKEN = "8589792528:AAHUoT5aVdLNlQpP2qRHUqdp0PDAXloboxQ"
     START_PIC = "https://te.legra.ph/file/e1b4148d7a4cd8b0832d9.jpg"
